4d2da9461d43d26ede028581e59f91fce4f4aaf7
marcus quigley <marcusquigleygmail.com>
2020-12-24 14:45:15 -0500

    Adding Banking Controller

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+ 
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/banking")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService) {
+            _accountService = accountService;
+        } 
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts() {
+            var accounts = await _accountService.GetAccountsAsync();
+            return Ok(accounts);
+        }
+    }
+}
